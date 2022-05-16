535f34723e52224e2992e5430934ea0c3f85a755
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2022-05-16 16:05:21 +0900

    Update switch.c

--- src/switch.c
+++ src/switch.c
@@ -7,10 +7,13 @@ int main() {
   switch(x){
     case 1:
       printf("1");
+      break;
     case 2:
       printf("2");
+      break;
     case 3:
       printf("3");
+      break;
     default:
     printf("error");
   }
